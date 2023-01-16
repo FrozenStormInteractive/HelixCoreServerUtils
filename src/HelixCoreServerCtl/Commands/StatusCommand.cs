e5eddbff526e959234a73fed36291e289ca34cd3
Yoann Potinet <yoann.potinet@frozenstorminteractive.com>
2023-01-15 22:59:15 -0500

    Refactor status command

--- src/HelixCoreServerCtl/Commands/StatusCommand.cs
+++ src/HelixCoreServerCtl/Commands/StatusCommand.cs
@@ -5,32 +5,66 @@ namespace HelixCoreServerCtl;
 [Verb("status")]
 internal class StatusCommand : ICommand
 {
-    [Value(0, MetaValue = "<name>", Required = true)]
-    public string ServiceName { get; set; } = null!;
+    [Value(0, MetaValue = "<name>")]
+    public IEnumerable<string> ServiceNames { get; set; } = null!;
+
+    [Option('a', "all", Default = false, HelpText = "All servers.")]
+    public bool AllServices { get; set; }
 
     public int Execute()
     {
-        var service = ServiceManager.Instance.FindServiceByName(ServiceName);
-        if (service is not null)
+        IList<Service> services;
+        if (AllServices)
         {
-            if (!service.Config.Enabled)
-            {
-                Console.WriteLine($"'{service.Config.Name}' p4d service is disabled.");
-            }
-            else if (service.IsRunning)
-            {
-                Console.WriteLine($"'{service.Config.Name}' p4d service is running.");
-            }
-            else
-            {
-                Console.WriteLine($"'{service.Config.Name}' p4d service not running.");
-            }
-            return 0;
+            services = ServiceManager.Instance.GetAllServices().Where(x => x.Config.Enabled).ToList();
         }
         else
         {
-            Console.Error.WriteLine($"Service '{ServiceName}' not found.");
-            return 1;
+            List<Service> serviceList =  new List<Service>();
+            services = serviceList;
+            foreach (var serviceName in ServiceNames)
+            {
+                var service = ServiceManager.Instance.FindServiceByName(serviceName);
+                if (service is not null)
+                {
+                    if (service.Config.Enabled)
+                    {
+                        serviceList.Add(service);
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine($"Service '{serviceName}' not found.");
+                }
+            }
+        }
+
+        if (services.Count > 0)
+        {
+            int exitCode = 0;
+
+            foreach (var service in services)
+            {
+                if (!service.Config.Enabled)
+                {
+                    Console.WriteLine($"'{service.Config.Name}' p4d service is disabled.");
+                }
+                else if (service.IsRunning)
+                {
+                    Console.WriteLine($"'{service.Config.Name}' p4d service is running.");
+                }
+                else
+                {
+                    exitCode = 1;
+                    Console.WriteLine($"'{service.Config.Name}' p4d service not running.");
+                }
+            }
+
+            return exitCode;
+        }
+        else
+        {
+            return 4;
         }
     }
 }
